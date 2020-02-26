60a2f1b243ad49b9f2713fb56166e52edbfbf0c8
Simo Aleksandrov <simo3003@me.com>
2020-02-26 13:40:49 +0200

    better print

--- solver.go
+++ solver.go
@@ -11,13 +11,25 @@ type Sudoku [9][9]int
 // Print prints the sudoku
 func (s Sudoku) String() string {
 	b := &strings.Builder{}
-	for _, line := range s {
-		for _, num := range line {
-			if num == 0 {
-				fmt.Fprint(b, "- ")
-			} else {
-				fmt.Fprintf(b, "%d ", num)
+	for i, line := range s {
+		if i%3 == 0 {
+			fmt.Fprintln(b, "+---------+---------+---------+")
+		}
+		for j, num := range line {
+			if j%3 == 0 {
+				fmt.Fprint(b, "|")
 			}
+			if num == 0 {
+				fmt.Fprint(b, " - ")
+			} else {
+				fmt.Fprintf(b, " %d ", num)
+			}
+			if j == 8 {
+				fmt.Fprint(b, "|")
+			}
+		}
+		if i == 8 {
+			fmt.Fprint(b, "\n+---------+---------+---------+")
 		}
 		fmt.Fprintln(b)
 	}
