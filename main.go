6c80c1cff044105d0011195666023d82671d1047
Simo Aleksandrov <simo3003@me.com>
2020-03-01 01:09:55 +0200

    do we ebiten instead LOL

--- main.go
+++ main.go
@@ -1,39 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
-	"github.com/fr3fou/sudogo/sudoku"
+	"github.com/fr3fou/sugoku/sudoku"
+	"github.com/hajimehoshi/ebiten"
 )
 
-func main() {
-	board := sudoku.Sudoku{
-		{0, 0, 8 /**/, 0, 0, 2 /**/, 0, 5, 0},
-		{0, 4, 0 /**/, 0, 0, 5 /**/, 0, 0, 8},
-		{0, 3, 5 /**/, 6, 0, 0 /**/, 2, 0, 7},
-		/*									*/
-		{3, 0, 1 /**/, 0, 2, 0 /**/, 0, 0, 0},
-		{5, 0, 0 /**/, 0, 7, 0 /**/, 0, 0, 1},
-		{0, 0, 0 /**/, 5, 9, 0 /**/, 8, 0, 3},
-		/*									*/
-		{7, 0, 3 /**/, 0, 0, 4 /**/, 0, 9, 0},
-		{0, 0, 0 /**/, 2, 0, 0 /**/, 0, 8, 0},
-		{0, 5, 0 /**/, 0, 0, 0 /**/, 6, 0, 0},
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
 	}
+	return nil
+}
 
+func main() {
+	// board := sudoku.Sudoku{
+	// 	{0, 0, 8 /**/, 0, 0, 2 /**/, 0, 5, 0},
+	// 	{0, 4, 0 /**/, 0, 0, 5 /**/, 0, 0, 8},
+	// 	{0, 3, 5 /**/, 6, 0, 0 /**/, 2, 0, 7},
+	// 	/*									*/
+	// 	{3, 0, 1 /**/, 0, 2, 0 /**/, 0, 0, 0},
+	// 	{5, 0, 0 /**/, 0, 7, 0 /**/, 0, 0, 1},
+	// 	{0, 0, 0 /**/, 5, 9, 0 /**/, 8, 0, 3},
+	// 	/*									*/
+	// 	{7, 0, 3 /**/, 0, 0, 4 /**/, 0, 9, 0},
+	// 	{0, 0, 0 /**/, 2, 0, 0 /**/, 0, 8, 0},
+	// 	{0, 5, 0 /**/, 0, 0, 0 /**/, 6, 0, 0},
+	// }
+
+	// ch := make(chan sudoku.Cell)
+	// solve(board, ch)
+	// for _ = range ch {
+	// 	// fmt.Printf("%d at %d x %d\n", cell.Num, cell.X, cell.Y)
+	// }
+	if err := ebiten.Run(update, 900, 900, 1, "Sugoku"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func solve(board sudoku.Sudoku, ch chan sudoku.Cell) {
 	solver := sudoku.Solver{
 		Board: &board,
-		Cells: make(chan sudoku.Cell),
+		Cells: ch,
 	}
 
-	ch := make(chan sudoku.Sudoku)
-	go func() {
-		ch <- solver.Solve()
-	}()
-
-	for _ = range solver.Cells {
-		// fmt.Printf("%d at %d x %d\n", cell.Num, cell.X, cell.Y)
-	}
-
-	fmt.Println(<-ch)
+	go solver.Solve()
 }
