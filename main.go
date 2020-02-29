e6effe36609c718193ad694efaf8ea85b4f41186
Simo Aleksandrov <simo3003@me.com>
2020-03-01 00:02:10 +0200

    we grird now

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/fr3fou/sudogo/sudoku"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	CellSize = 100
+	White    = 0xFFFFFF
+	Black    = 0x000000
+)
+
 func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Sudoku", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		900, 900, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
 	board := sudoku.Sudoku{
 		{0, 0, 8 /**/, 0, 0, 2 /**/, 0, 5, 0},
 		{0, 4, 0 /**/, 0, 0, 5 /**/, 0, 0, 8},
@@ -22,43 +38,27 @@ func main() {
 		{0, 5, 0 /**/, 0, 0, 0 /**/, 6, 0, 0},
 	}
 
-	solver := sudoku.Solver{
-		Board: &board,
-		Cells: make(chan sudoku.Cell),
-	}
-
-	ch := make(chan sudoku.Sudoku)
-	go func() {
-		ch <- solver.Solve()
-	}()
-
-	for _ = range solver.Cells {
-		// fmt.Printf("%d at %d x %d\n", cell.Num, cell.X, cell.Y)
-	}
-
-	fmt.Println(<-ch)
-
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
-	}
-	defer sdl.Quit()
-
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
-	}
-	defer window.Destroy()
-
 	surface, err := window.GetSurface()
 	if err != nil {
 		panic(err)
 	}
-	surface.FillRect(nil, 0)
+	surface.FillRect(nil, Black) // draw bg
 
-	rect := sdl.Rect{0, 0, 200, 200}
-	surface.FillRect(&rect, 0xffff0000)
+	for x, line := range board {
+		for y, _ := range line {
+			rect := sdl.Rect{
+				X: int32(x*CellSize) + 1,
+				Y: int32(y*CellSize) + 1,
+				W: CellSize - 2,
+				H: CellSize - 2,
+			}
+			surface.FillRect(&rect, White)
+		}
+	}
+
 	window.UpdateSurface()
+	// ch := make(chan sudoku.Cell)
+	// solve(board, ch)
 
 	running := true
 	for running {
@@ -72,4 +72,13 @@ func main() {
 		}
 	}
 
+}
+
+func solve(board sudoku.Sudoku, ch chan sudoku.Cell) {
+	solver := sudoku.Solver{
+		Board: &board,
+		Cells: ch,
+	}
+
+	go solver.Solve()
 }
