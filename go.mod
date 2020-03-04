1698c74a6e9afa4cbe3cfc27b96372be05f8249a
Simo Aleksandrov <simo3003@me.com>
2020-03-04 18:11:52 +0200

    Revert "Revert "we sdl now""
    This reverts commit ef7238ae7510d42c849945f578670c615efdb372.

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/davecgh/go-spew v1.1.0
 	github.com/go-playground/assert/v2 v2.0.1
 	github.com/stretchr/testify v1.5.1
+	github.com/veandco/go-sdl2 v0.4.1
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/fr3fou/sudogo/sudoku"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
@@ -36,4 +37,39 @@ func main() {
 	}
 
 	fmt.Println(<-ch)
+
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	surface.FillRect(&rect, 0xffff0000)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
+
 }
